59e095fdfc389135d0d26548a2ebad439301c3d6
Dmitry Mazo <dmazo@uma.tech>
2021-01-02 19:16:58 +0300

    Next/previous

--- apps/sailboatRadio/SailboatRadio/Sources/browse/BrowseView.swift
+++ apps/sailboatRadio/SailboatRadio/Sources/browse/BrowseView.swift
@@ -15,8 +15,10 @@ struct BrowseView: View {
     private let urls = [
         "https://masima.rastream.com/masima-pramborsjakarta?awparams=stationid:masima-pramborsjakarta",
         "https://0n-country.radionetz.de/0n-country.aac",
+        "https://18193.live.streamtheworld.com/SAM03AAC226_SC",
+        "https://19353.live.streamtheworld.com/977_HITS_SC",
+        "https://18853.live.streamtheworld.com/977_SMOOJAZZ_SC",
         "https://streams.calmradio.com:4428/stream",
-        "https://18193.live.streamtheworld.com/SAM03AAC226_SC"
     ]
     
     @State
@@ -24,27 +26,29 @@ struct BrowseView: View {
     
     var body: some View {
         VStack(spacing: 50) {
-            HStack(spacing: 30) {
-                if currentUrlNo > 0 {
-                    Button("Load Previous") {
-                        guard currentUrlNo > 0 else { return }
-                        
-                        let url = urls[currentUrlNo]
-                        try? player.load(url: url)
+            Text("\(currentUrlNo)")
+            VStack(spacing: 30) {
+                Button("Load Previous") {
+                    guard currentUrlNo > 0 else { return }
+                    
+                    let url = urls[currentUrlNo]
+                    if currentUrlNo >= 0 {
                         currentUrlNo -= 1
-                        player.play()
                     }
-                }
-                if currentUrlNo < urls.count {
-                    Button("Load Next") {
-                        guard currentUrlNo < urls.count else { return }
-                        
-                        let url = urls[currentUrlNo]
-                        try? player.load(url: url)
+                    try? player.load(url: url)
+                    player.play()
+                }.disabled(currentUrlNo == 0)
+                
+                Button("Load Next") {
+                    guard currentUrlNo < urls.count else { return }
+                    
+                    let url = urls[currentUrlNo]
+                    if currentUrlNo < urls.count - 1 {
                         currentUrlNo += 1
-                        player.play()
                     }
-                }
+                    try? player.load(url: url)
+                    player.play()
+                }.disabled(currentUrlNo == urls.count - 1)
             }
             player.view
         }
